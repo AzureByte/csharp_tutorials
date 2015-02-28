193b1aede096566ff84a61a0b9c57fd806e78eff
Neville Bokdawalla <nevillekb@gmail.com>
2015-02-28 21:21:53 +0530

    added class file to demonstrate the 'for' loop

---
+++ csharp_tutorials/src/05_for_loop_demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp_tutorials.src
+{
+    class for_loop_demo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("counting from 1 to 10");
+
+            //start from 1 and count till 10 
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("counting..." + i);
+            }
+
+            Console.WriteLine("Done counting...");
+        }
+    }
+}
