678fb3e026e427b788eb463efed2574fe357969d
Neville Bokdawalla <nevillekb@gmail.com>
2015-02-28 19:51:14 +0530

    committed the following files 1. app.config 2. assemblyinfo.cs 3. csproj file 4. solution file 5. the_first_program.cs ( which gives a gentle introduction to c# with a sample demo program )

---
+++ csharp_tutorials/src/the_first_program.cs
@@ -0,0 +1,16 @@
+﻿//a simple c# program that prints a message to the console.
+
+//use the 'system' namespace
+using System;
+
+namespace csharp_tutorials.src
+{
+    class the_first_program
+    {
+        //every c# program begins with a call to the Main() method
+        public static void Main()
+        {
+            Console.WriteLine("c# gives me programming power...!");
+        }
+    }
+}
