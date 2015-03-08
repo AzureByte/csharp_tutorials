a86df1a267932261a6ac31ab7ea7e47f79e5a8d2
Neville Bokdawalla <nevillekb@gmail.com>
2015-03-08 19:43:45 +0530

    added class for demonstrating the directory and directoryInfo classes

---
+++ csharp_tutorials/src/File IO/03_Directory_and_DirectoryInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace csharp_tutorials.src.File_IO
+{
+    class _03_Directory_and_DirectoryInfo
+    {
+        public static void Main()
+        {
+            string directoryName = "testDirectory";
+
+            if (!Directory.Exists(@"c:\users\neville\desktop\" + directoryName))
+            {
+                DirectoryInfo dInfo = new DirectoryInfo(@"c:\users\neville\desktop\" + directoryName);
+                dInfo.Create();
+            }
+        }
+    }
+}
