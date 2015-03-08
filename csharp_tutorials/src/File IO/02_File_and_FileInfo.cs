a1f4fb5918cdc33b97fdc95182f6eb1fea11f1a4
Neville Bokdawalla <nevillekb@gmail.com>
2015-03-08 19:23:53 +0530

    added new class that demonstrates the File and FileInfo classes

---
+++ csharp_tutorials/src/File IO/02_File_and_FileInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace csharp_tutorials.src.File_IO
+{
+    class _02_File_and_FileInfo
+    {
+        public static void Main()
+        {
+            string fileName = "testFile.txt";
+
+            if (!File.Exists(@"c:\users\neville\desktop\" + fileName))
+            {
+                FileInfo fInfo = new FileInfo(@"c:\users\neville\desktop\" + fileName);
+                FileStream fStream = fInfo.Create();
+                fStream.Close();
+            }
+        }
+    }
+}
