c0a84140ad1670f4a331a88a2c86162e97f241a6
neville bokdawalla <nevillekb@gmail.com>
2016-01-31 00:06:46 +0530

    - updated file names - added the byte and sbyte code samples

---
+++ csharp_tutorials/src/03_Data Types/02_byte.cs
@@ -0,0 +1,24 @@
+﻿/*
+    This program demonstrates the 'byte' data type.
+*/
+
+using System;
+
+namespace csharp_tutorials.src._03_Data_Types
+{
+    class byte_data_type
+    {
+       public static void Main()
+        {
+            byte b = 129;
+            
+            Console.WriteLine("value contained in the variable 'b' = " + b);
+            Console.WriteLine();
+
+            Console.WriteLine("minimum value that a 'byte' data type can hold = " + byte.MinValue);
+            Console.WriteLine();
+
+            Console.WriteLine("maximum value that a 'byte' data type can hold = " + byte.MaxValue);
+        }
+    }
+}
---
+++ csharp_tutorials/src/03_Data Types/03_signed_byte.cs
@@ -0,0 +1,29 @@
+﻿/*
+    This program demonstrates the 'signed byte' data type
+*/
+
+using System;
+
+namespace csharp_tutorials.src._03_Data_Types
+{
+    class _03_signed_byte
+    {
+        public static void Main()
+        {
+            sbyte sb1 = 103;
+            sbyte sb2 = -99;
+
+            Console.WriteLine("value contained in the variable 'sb1' = " + sb1);
+            Console.WriteLine();
+
+            Console.WriteLine("value contained in the variable 'sb2' = " + sb2);
+            Console.WriteLine();
+
+            Console.WriteLine("minimum value that an 'sbyte' variable can hold = " + sbyte.MinValue);
+            Console.WriteLine();
+
+            Console.WriteLine("maximum value that an 'sbyte' variable can hold = " + sbyte.MaxValue);
+            Console.WriteLine();
+        }
+    }
+}
