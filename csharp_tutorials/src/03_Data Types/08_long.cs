15b45d57b81d1371b6a63f4afbbc4009e0599151
neville bokdawalla <nevillekb@gmail.com>
2016-01-31 20:02:03 +0530

    added long and ulong code samples

---
+++ csharp_tutorials/src/03_Data Types/08_long.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp_tutorials.src._03_Data_Types
+{
+    class _08_long
+    {
+        public static void Main()
+        {
+            long a = 123456789;
+
+            Console.WriteLine("value contained in the variable 'a' = " + a);
+            Console.WriteLine();
+
+            Console.WriteLine("minimum value that a 'long' data type can hold = " + long.MinValue);
+            Console.WriteLine();
+
+            Console.WriteLine("maximum value that a 'long' data type can hold = " + long.MaxValue);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ csharp_tutorials/src/03_Data Types/09_ULong.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp_tutorials.src._03_Data_Types
+{
+    class _09_ULong
+    {
+        public static void Main()
+        {
+            ulong a = 98776654;
+
+            Console.WriteLine("value contained in the variable 'a' = " + a);
+            Console.WriteLine();
+
+            Console.WriteLine("minimum value that a 'ulong' data type can hold = " + ulong.MinValue);
+            Console.WriteLine();
+
+            Console.WriteLine("maximum value that a 'ulong' data type can hold = " + ulong.MaxValue);
+            Console.WriteLine();
+        }
+    }
+}
